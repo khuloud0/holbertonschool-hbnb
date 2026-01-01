b6bd6fa416e494045df3d047c4454ed738a4cd8d
khuloud0 <khuloodalgarni@gmail.com>
2026-01-01 12:32:23 +0300

    Update place.py

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+from app.models.base_model import BaseModel
 
+class Place(BaseModel):
+    def __init__(self, title="", description="", price=0.0,
+                 latitude=0.0, longitude=0.0, owner_id=""):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner_id = owner_id
