14a62af3e89a97500278bc8a7bdc9770c39ee191
khuloud0 <khuloodalgarni@gmail.com>
2026-01-01 12:34:52 +0300

    Update user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
 
+from app.models.base_model import BaseModel
+
+class User(BaseModel):
+    def __init__(self, email: str, first_name: str = "", last_name: str = "", password: str = "", is_admin: bool = False):
+        super().__init__()
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.is_admin = is_admin
