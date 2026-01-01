5d67570968eaf8d4d1e973e825bf4dda2489ba49
khuloud0 <khuloodalgarni@gmail.com>
2026-01-01 12:33:47 +0300

    Update review.py

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
 
+from app.models.base_model import BaseModel
+
+class Review(BaseModel):
+    def __init__(self, text: str, user_id: str, place_id: str):
+        super().__init__()
+        self.text = text
+        self.user_id = user_id
+        self.place_id = place_id
