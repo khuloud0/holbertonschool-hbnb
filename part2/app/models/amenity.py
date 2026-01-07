d51ff9b7b509e6b6efb86fbcf688af36b48d0e60
khuloud0 <khuloodalgarni@gmail.com>
2026-01-07 14:44:00 +0300

    Update amenity.py

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -5,28 +5,3 @@ class Amenity(BaseModel):
         super().__init__()
         self.name = name
         self.description = description
-
-  def to_dict(self):
-        """Convert object to dictionary"""
-        base_dict = super().to_dict() if hasattr(super(), 'to_dict') else {}
-        return {
-            **base_dict,
-            "id": self.id,
-            "name": self.name,
-            "description": self.description
-        }
-    
-    def update(self, data: dict):
-        """Update amenity data"""
-        if 'name' in data:
-            self.name = data['name']
-        if 'description' in data:
-            self.description = data['description']
-    
-    @classmethod
-    def from_dict(cls, data: dict):
-        """Create object from dictionary"""
-        return cls(
-            name=data.get('name', ''),
-            description=data.get('description', '')
-        )
