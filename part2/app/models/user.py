3653bdb74eee0a88b04056612950409f5ed673dd
khuloud0 <khuloodalgarni@gmail.com>
2026-01-07 14:26:59 +0300

    Update user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -10,36 +10,3 @@ class User(BaseModel):
         self.last_name = last_name
         self.password = password
         self.is_admin = is_admin
-
-        # Merge dictionaries, excluding password
-        return {**base_dict, **user_dict}
-    
-    def to_dict_with_password(self):
-        """Convert user to dictionary (include password - for internal use)"""
-        base_dict = super().to_dict()
-        user_dict = {
-            "email": self.email,
-            "password": self.password,
-            "first_name": self.first_name,
-            "last_name": self.last_name
-        }
-        return {**base_dict, **user_dict}
-    
-    def update(self, data: dict):
-        """Update user attributes"""
-        allowed_fields = ['email', 'first_name', 'last_name']
-        for field in allowed_fields:
-            if field in data:
-                setattr(self, field, data[field])
-        super().save()
-        return self
-    
-    @classmethod
-    def from_dict(cls, data: dict):
-        """Create user instance from dictionary"""
-        return cls(
-            email=data.get('email', ''),
-            password=data.get('password', ''),
-            first_name=data.get('first_name', ''),
-            last_name=data.get('last_name', '')
-        )
