c3b200240aef0f3646c34797f7de9be8fb3a54ed
khuloud0 <khuloodalgarni@gmail.com>
2026-01-09 20:29:19 +0300

    Update base_model.py

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -1,46 +1,21 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, **kwargs):
+        self.id = kwargs.get("id", str(uuid.uuid4()))
+        self.created_at = kwargs.get("created_at", datetime.utcnow())
+        self.updated_at = kwargs.get("updated_at", datetime.utcnow())
 
-    def save(self):
-        self.updated_at = datetime.utcnow()
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
 
-  @classmethod
-    def from_dict(cls, data: dict):
-        """Create an instance from a dictionary (factory method)"""
-        # Create instance without calling __init__ to avoid double ID generation
-        instance = cls.__new__(cls)
-        
-        # Set attributes from dictionary
-        for key, value in data.items():
-            # Convert string dates back to datetime objects
-            if key in ['created_at', 'updated_at'] and isinstance(value, str):
-                try:
-                    value = datetime.fromisoformat(value)
-                except ValueError:
-                    # Fallback for other date formats if needed
-                    pass
-            setattr(instance, key, value)
-        
-        return instance
-    
-    def __str__(self):
-        """String representation of the object"""
-        class_name = self.__class__.__name__
-        attrs = {}
-        
-        # Get all non-callable attributes that don't start with underscore
-        for key in dir(self):
-            if not key.startswith('_') and not callable(getattr(self, key)):
-                attrs[key] = getattr(self, key)
-        
-        return f"[{class_name}] ({self.id}) {attrs}"
-    
-    def __repr__(self):
-        """Official string representation"""
-        return self.__str__()
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
