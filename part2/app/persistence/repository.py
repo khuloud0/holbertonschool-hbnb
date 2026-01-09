a2b072c92636eda1985b532e1e3b211dd7f788b9
khuloud0 <khuloodalgarni@gmail.com>
2026-01-09 20:27:02 +0300

    Update repository.py

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,1 +1,35 @@
+#!/usr/bin/python3
 
+class InMemoryRepository:
+    """Simple in-memory repository for storing objects"""
+
+    def __init__(self):
+        self._storage = {}
+
+    def add(self, obj):
+        """Add an object to the repository"""
+        self._storage[obj.id] = obj
+        return obj
+
+    def get(self, obj_id):
+        """Retrieve an object by ID"""
+        return self._storage.get(obj_id)
+
+    def get_all(self):
+        """Retrieve all objects"""
+        return list(self._storage.values())
+
+    def update(self, obj_id, data):
+        """Update an object"""
+        obj = self._storage.get(obj_id)
+        if not obj:
+            return None
+
+        for key, value in data.items():
+            if hasattr(obj, key):
+                setattr(obj, key, value)
+        return obj
+
+    def delete(self, obj_id):
+        """Delete an object"""
+        return self._storage.pop(obj_id, None)
