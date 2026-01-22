a1c668e3b2bdc9f66ff40d0426de47972db66989
khuloud0 <khuloodalgarni@gmail.com>
2026-01-22 16:10:44 +0300

    Update amenity.py

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,7 +1,23 @@
+#!/usr/bin/python3
+"""Amenity model"""
+
 from app.models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-    def __init__(self, name: str, description: str = ""):
+    """Amenity class"""
+
+    def __init__(self, name: str):
         super().__init__()
+
+        # ---- validation ----
+        self._validate_name(name)
+
+        # ---- assign value ----
         self.name = name
-        self.description = description
+
+    def _validate_name(self, name):
+        if not name or not isinstance(name, str):
+            raise ValueError("name is required")
+        if len(name) > 50:
+            raise ValueError("name must be at most 50 characters")
