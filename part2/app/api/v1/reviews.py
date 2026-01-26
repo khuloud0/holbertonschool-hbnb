23440d53d8915f08a202e2b622ec6faedb4863c5
khuloud0 <khuloodalgarni@gmail.com>
2026-01-26 16:13:59 +0300

    Update reviews.py

--- part2/app/api/v1/reviews.py
+++ part2/app/api/v1/reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask_restx import Namespace, Resource, fields
-from app.services.facade import HBnBFacade
+from app.services.facade import facade
 
 api = Namespace('reviews', description='Review operations')
 facade = HBnBFacade()
