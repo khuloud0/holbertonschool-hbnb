234cf0a546a0e9388d7da9a239071194386d033e
Layan-6 <11956@holbertonstudents.com>
2026-01-28 01:14:04 +0300

    Initialize models package with imports

---
+++ Part3/hbnb/app/models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Models package initialization."""
+
+from .base_model import BaseModel
+from .user import User
+from .place import Place
+from .review import Review
+from .amenity import Amenity
+
+__all__ = ['BaseModel', 'User', 'Place', 'Review', 'Amenity']
