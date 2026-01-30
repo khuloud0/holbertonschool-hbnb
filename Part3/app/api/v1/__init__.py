cb420d4bbfc73879e16bad73ae87c0b98a5472a0
khuloud0 <khuloodalgarni@gmail.com>
2026-01-30 17:45:38 +0300

    Update __init__.py

--- Part3/app/api/v1/__init__.py
+++ Part3/app/api/v1/__init__.py
@@ -1,1 +1,12 @@
+from flask_restx import Api
+from app.api.v1.users import api as users_ns
+from app.api.v1.auth import api as auth_ns
 
+api = Api(
+    title='HBnB API',
+    version='1.0',
+    description='HBnB REST API'
+)
+
+api.add_namespace(users_ns, path='/users')
+api.add_namespace(auth_ns, path='/auth')
