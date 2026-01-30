34d7d75d0ec1408ab9b18b6a701b3a90eb68cb42
khuloud0 <khuloodalgarni@gmail.com>
2026-01-30 17:30:37 +0300

    Update run.py

--- Part3/run.py
+++ Part3/run.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python3
 from app import create_app
-from config import config
-import os
 
-# Get environment or default to development
-env = os.environ.get('FLASK_ENV', 'default')
-app = create_app(config[env])
+app = create_app()
 
 if __name__ == "__main__":
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port, debug=app.config['DEBUG'])
+    app.run()
