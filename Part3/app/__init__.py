8a55eaf15eda400728339b3107e7a97adda070cb
khuloud0 <khuloodalgarni@gmail.com>
2026-01-30 17:31:18 +0300

    Update __init__.py

--- Part3/app/__init__.py
+++ Part3/app/__init__.py
@@ -1,1 +1,17 @@
+class Config:
+    """Base configuration class"""
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "super-secret-key"
 
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
