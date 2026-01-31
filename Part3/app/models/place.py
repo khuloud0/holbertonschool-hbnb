b6a5dd99f83142f829b362d883122436cd5eee39
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 09:27:14 +0300

    Update place.py

--- Part3/app/models/place.py
+++ Part3/app/models/place.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""Place model"""
 
+from app.db import db
+from app.models.base_model import BaseModel
+
+
+class Place(BaseModel, db.Model):
+    """Place class"""
+
+    __tablename__ = "places"
+
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1024))
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
