c586840cad547ec97ee448c6a3474f858d1fe319
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 09:28:24 +0300

    Update review.py

--- Part3/app/models/review.py
+++ Part3/app/models/review.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+from app.db import db
+from app.models.base_model import BaseModel
 
+
+class Review(BaseModel, db.Model):
+    __tablename__ = "reviews"
+
+    text = db.Column(db.String(1024), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
