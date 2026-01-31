6a555396ceed89311ce7f8fb0ad78c8f9ab33a0b
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 10:19:32 +0300

    Update amenities.py

--- Part3/app/api/v1/amenities.py
+++ Part3/app/api/v1/amenities.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python3
+"""Amenities API"""
 
+from flask_restx import Namespace, Resource, fields
+from app.services.facade import facade
+
+api = Namespace("amenities", description="Amenities operations")
+
+amenity_model = api.model(
+    "Amenity",
+    {
+        "id": fields.String(readonly=True),
+        "name": fields.String(required=True),
+        "description": fields.String(required=True),
+        "created_at": fields.DateTime,
+        "updated_at": fields.DateTime,
+    },
+)
+
+
+@api.route("/")
+class AmenityList(Resource):
+    @api.marshal_list_with(amenity_model)
+    def get(self):
+        return facade.get_all_amenities()
+
+    @api.expect(amenity_model, validate=True)
+    @api.marshal_with(amenity_model, code=201)
+    def post(self):
+        return facade.create_amenity(api.payload)
+
+
+@api.route("/<amenity_id>")
+class AmenityResource(Resource):
+    @api.marshal_with(amenity_model)
+    def get(self, amenity_id):
+        amenity = facade.get_amenity(amenity_id)
+        if not amenity:
+            api.abort(404, "Amenity not found")
+        return amenity
