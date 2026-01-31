39c84eaae05aa4e5cfdd1ce45931836b839cc951
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 09:36:30 +0300

    Update review.py

--- Part3/app/models/review.py
+++ Part3/app/models/review.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+"""Review model"""
+
 from app.db import db
 from app.models.base_model import BaseModel
 
 
 class Review(BaseModel, db.Model):
+    """Review class"""
+
     __tablename__ = "reviews"
 
+    # ===== Columns =====
     text = db.Column(db.String(1024), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
+
+    # ===== Foreign Keys =====
+    user_id = db.Column(
+        db.String(60),
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    place_id = db.Column(
+        db.String(60),
+        db.ForeignKey("places.id"),
+        nullable=False
+    )
