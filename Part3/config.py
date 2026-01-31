935d687f388801780db80e698ce846c01faaa4cf
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 10:21:48 +0300

    Update config.py

--- Part3/config.py
+++ Part3/config.py
@@ -4,14 +4,19 @@ class Config:
     TESTING = False
     SECRET_KEY = "super-secret-key"
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb_dev.db"
 
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb_test.db"
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb_prod.db"
