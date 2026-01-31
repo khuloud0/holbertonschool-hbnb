2375a7e34008dc7933a1a31fd67d385ec59268be
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 09:29:35 +0300

    Update amenity.py

--- Part3/app/models/amenity.py
+++ Part3/app/models/amenity.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+from app.db import db
+from app.models.base_model import BaseModel
 
+
+class Amenity(BaseModel, db.Model):
+    __tablename__ = "amenities"
+
+    name = db.Column(db.String(128), nullable=False, unique=True)
+    description = db.Column(db.String(1024), nullable=False)
