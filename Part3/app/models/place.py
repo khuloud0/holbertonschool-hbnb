838735a061f2ff6f8e9d1639dce31a502afcfb22
khuloud0 <khuloodalgarni@gmail.com>
2026-01-31 09:34:57 +0300

    Update place.py

--- Part3/app/models/place.py
+++ Part3/app/models/place.py
@@ -3,6 +3,7 @@
 
 from app.db import db
 from app.models.base_model import BaseModel
+from app.models.place_amenity import place_amenity
 
 
 class Place(BaseModel, db.Model):
@@ -10,8 +11,30 @@ class Place(BaseModel, db.Model):
 
     __tablename__ = "places"
 
+    # ===== Columns =====
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(1024))
     price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+
+    # ===== Foreign Keys =====
+    owner_id = db.Column(
+        db.String(60),
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    # ===== Relationships =====
+    reviews = db.relationship(
+        "Review",
+        backref="place",
+        cascade="all, delete-orphan"
+    )
+
+    amenities = db.relationship(
+        "Amenity",
+        secondary=place_amenity,
+        backref="places",
+        lazy="subquery"
+    )
